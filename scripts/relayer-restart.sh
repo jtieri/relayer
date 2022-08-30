78ea43552e90ef0782964ad72c28b1fc33fd1904
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2022-08-30 11:21:43 -0500

    Update relayer-restart.sh
    add restart command for agoric<>cosmoshub

--- scripts/relayer-restart.sh
+++ scripts/relayer-restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script is used to restart the running systemd servcies related to running relayer instances.
+sudo systemctl restart rly-agoric-cosmoshub
 sudo systemctl restart rly-axelar-crescent
 sudo systemctl restart rly-axelar-juno
 sudo systemctl restart rly-axelar-osmosis
